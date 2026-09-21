8b896672d2af81695081676b1b4a92b427458700
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-09-20 21:52:03 -0300

    feat: agrega Graphs.c para implementaciones del header Graphs.h

---
+++ src/Graphs.c
@@ -0,0 +1,11 @@
+
+#include <Graphs.h>
+
+
+BinomialTreeForest32* initBinomialQueue32(uint32_t nodeCount, const double* nodeValues, BinomialTreeForest32* dest) {
+    return dest; 
+}
+
+BinomialTreeForest32* initFiboQueue32(uint32_t nodeCount, const double* nodeValues, BinomialTreeForest32* dest) {
+    return dest; 
+}
