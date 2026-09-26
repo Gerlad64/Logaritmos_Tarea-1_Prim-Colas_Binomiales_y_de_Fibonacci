bf961b1c40b9b2581b0637e06bb292ce0ac90305
Gerlad64 <gerald.ponce@ug.uchile.cl>
2026-09-26 13:41:06 -0300

    feat: Añade Graphs.test.c para tests de Graphs.h y Graphs.c
    Agrega plantilla para rellenar con tests

---
+++ tests/Graphs.test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <assert.h> // assert(int expression)
+
+
+void test_initBinomialQueue32() {
+    
+}
+
+void test_initFiboQueue32() {
+
+
+}
+
+void test_extractMin_Binomial32() {
+    
+}
+
+void test_extractMin_Fibo32() {
+    
+}
+
+void test_decreaseKey_Binomial32() {
+    
+}
+
+void test_decreaseKey_Fibo32() {
+    
+}
+
+int main() {
+    printf("--------TESTS Graphs.h/Graphs.c-------\n");
+    
+    test_initBinomialQueue32();
+    test_initFiboQueue32();
+    test_extractMin_Binomial32();
+    test_extractMin_Fibo32();
+    test_decreaseKey_Binomial32();
+    test_decreaseKey_Fibo32();
+
+    printf("--------TODOS LOS TESTS PASARON-------\n");
+    return 0;
+}
